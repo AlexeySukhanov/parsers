17788f9397a5601ea4be064d84487e34162c6aa1
sukhanov <webdevforbiz@gmail.com>
2019-09-15 03:36:26 +0300

    basic done + vk status functions

--- basic/basic.php
+++ basic/basic.php
@@ -1,1 +1,43 @@
 <?php
+
+/* Парсер статуса вконтакте */
+/*
+// Исходные данные:
+$url = 'https://vk.com/id4345432';  // Адрес страницы vk
+$start_marker = '<div class="pp_status">';      // Теги отмечающие начало строки (удаляются из конечного результата)
+$end_marker = '</div>';                         // Теги отмечающие конец строки (удаляются из конечного результата)
+
+// Необходимые вычисления:
+$content = file_get_contents( $url );
+$result = strip_tags( $content, '<div>, <img>' ); // очистка тегов необходимая для того чтобы js не отрабатывал при опытке отобразить страницу
+
+$start_position = strpos( $result, $start_marker );
+$result = substr( $result, $start_position );
+
+$end_position = strpos( $result, $end_marker );
+$start_marker_length = strlen( $start_marker );
+$result = substr( $result, 0, $end_position );
+$result = str_replace( $start_marker, '', $result );
+
+echo $result;
+*/
+
+
+/* Парсер статуса вконтакте с помощью regExp */
+/*
+// Исходные данные
+$url = 'https://vk.com/id10120294';  // Адрес страницы vk
+$start_marker = '<div class="pp_status">'; // Теги отмечающие начало строки (удаляются из конечного результата)
+$end_marker = '</div>'; // Теги отмечающие конец строки (удаляются из конечного результата)
+
+$content = file_get_contents( $url );
+
+$pattern = '#' . $start_marker . '.*' . $end_marker .'#';
+$count = preg_match( $pattern, $content, $result );
+$result = strip_tags( $result[0] );
+
+echo $result;
+*/
+
+
+
---
+++ vk_status/vk_status.php
@@ -0,0 +1,37 @@
+<?php
+
+/* Вывод списка с заданном количеством статусов vk  */
+
+function get_vk_status( $url ) {
+    if ( $content = file_get_contents( $url )  ) {
+
+        $start_marker = '<div class="pp_status">';
+        $end_marker = '</div>';
+
+        if ( strpos( $content, $start_marker ) ) {
+            $pattern = '#' . $start_marker . '.*' . $end_marker .'#';
+            preg_match( $pattern, $content, $result );
+            $result = strip_tags( $result[0] );
+            return $url . ' : '.$result;
+        } else return false;
+    } else return false;
+}
+
+function print_vk_statuses(  $start_position = 0, $count = 10 ) {
+    $i = 0;
+    $id = $start_position;
+    while ($i < $count ) {
+        $url = 'https://vk.com/id' . $id;
+        if ( $result = get_vk_status( $url ) ) {
+            echo $result;
+            echo '<br>';
+            $i++;
+        }
+        $id++;
+    }
+}
+
+echo '<h1 style="text-align: center">💅 Золотой фонд цитат VK 🤳</h1>';
+echo '<br>';
+
+print_vk_statuses( 2000);
