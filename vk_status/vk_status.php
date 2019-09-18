4a8b064d37d3c7db31cd09899314a103734f0cea
sukhanov <webdevforbiz@gmail.com>
2019-09-18 16:24:53 +0300

    add statements

--- vk_status/vk_status.php
+++ vk_status/vk_status.php
@@ -9,15 +9,14 @@ function get_vk_status( $url ) {
         $end_marker = '</div>';
 
         if ( strpos( $content, $start_marker ) ) {
-            $pattern = '#' . $start_marker . '.*' . $end_marker .'#';
+            $pattern = '#(?<=' . $start_marker . ').*(?=' . $end_marker .')#';
             preg_match( $pattern, $content, $result );
-            $result = strip_tags( $result[0] );
-            return $url . ' : '.$result;
+            return $url . ' : '.$result[0];
         } else return false;
     } else return false;
 }
 
-function print_vk_statuses(  $start_position = 0, $count = 10 ) {
+function print_vk_statuses(  $start_position = 0, $count = 4 ) {
     $i = 0;
     $id = $start_position;
     while ($i < $count ) {
